cea80225f54b7fee93dce797300a052330afa51b
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-28 21:57:38 -0500

    p

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = requier("cors");
+const cors = require("cors");
 const controller = require("./controller/control");
 
 const PORT = process.env.PORT  || 3000;
