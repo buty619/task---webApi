d0bbc01783600f44221e8006708d46c8ad0556ae
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-28 21:51:52 -0500

    cors intall

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = requiere("cors");
 const controller = require("./controller/control");
 
 const PORT = process.env.PORT  || 3000;
@@ -9,6 +10,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/top_dev',
 mongoose.connection.on("error", function(e) { console.error(e); });
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
 
 
