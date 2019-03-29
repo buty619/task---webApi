337a6550258322b56ceaa5add28de2ede6d3a175
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2019-03-28 20:12:32 -0500

    post taskAPi

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const controller = require("./controller/control");
 
-
-mongoose.connect('mongodb+srv://CristianB:cristian1991@cluster0-vjfaj.mongodb.net/taskApi?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/top_dev', { useNewUrlParser: true });
+//mongoose.connect('mongodb+srv://CristianB:cristian1991@cluster0-vjfaj.mongodb.net/taskApi?retryWrites=true', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 app.use(express.json());
 app.use(express.urlencoded());
@@ -16,4 +16,5 @@ app.post("/todo_items", controller.create);
 app.patch('/todo_items/:id', controller.update);
 app.delete('/todo_items/:id', controller.delete);
 
-app.listen(3000, () => console.log("Inició en puerto 3000 ..."));
+//app.listen(3000, () => console.log("Inició en puerto 3000 ..."));
+app.listen(PORT, () => console.log("Inició en puerto .." + PORT));
