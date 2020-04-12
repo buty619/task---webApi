1f942b7c0c3418c2466ef42f621ec9eedd490dc1
Cristian Felipe Buitrago Rodriguez <36422189+buty619@users.noreply.github.com>
2020-04-11 21:07:31 -0500

    adding mailing

--- controller/control.js
+++ controller/control.js
@@ -1,44 +1,79 @@
 const app = require('express')();
 const Task = require("../model/Task");
+const SparkPost = require('sparkpost');
+const client = new SparkPost(process.env.MAILING);
 
-exports.findAll = async (req,res) =>{
-  try{
+exports.findAll = async (req, res) =>
+{
+  try
+  {
     const listofTask = await Task.find();
     res.json(listofTask);
-  }catch(e){
+  } catch (e)
+  {
     console.error(e);
   }
 }
 
-exports.create = async (req,res) =>{
+exports.create = async (req, res) =>
+{
   console.log(req.body);
   await Task.create(req.body);
   res.status(204).send({});
 }
 
-exports.update = async (req,res) =>{
+exports.update = async (req, res, next) =>
+{
   const id = req.params.id;
-  const task = await  Task.findById(id);
+  const task = await Task.findById(id);
   task.title = req.body.title;
   task.done = req.body.done;
   task.updated_at = Date.now();
-  try{
+  try
+  {
     await task.save({});
     res.status(204).send({});
-  }catch(e){
+  } catch (e)
+  {
     return next(e);
   }
 }
 
-exports.delete = async (req,res) =>{
+exports.delete = async (req, res) =>
+{
   const id = req.params.id;
-  const task = await  Task.findById(id);
-  try{
-      task.remove(function(err) {
+  const task = await Task.findById(id);
+  try
+  {
+    task.remove(function (err)
+    {
       if (err) return console.error(err);
-    });  
+    });
     res.status(204).send({});
-  }catch(e){
+  } catch (e)
+  {
+    return next(e);
+  }
+}
+
+exports.mail = async (req, res) =>
+{
+  const params = req.params;
+  try
+  {
+    client.transmissions
+      .send({
+        content: {
+          from: 'cm-punk-619@hotmail.com',
+          subject: 'Hello, World!',
+          html:
+            "<html><body><p>My cool email.</p></body></html>"
+        },
+        recipients: [{ address: 'cm-punk-619@hotmail.com' }]
+      }).then(res.status(204).send({}))
+  }
+  catch (e)
+  {
     return next(e);
   }
 }
--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const app = express();
 const cors = require("cors");
 const controller = require("./controller/control");
 
-const PORT = process.env.PORT  || 3000;
+const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/top_dev', { useNewUrlParser: true });
 //mongoose.connect('mongodb+srv://CristianB:cristian1991@cluster0-vjfaj.mongodb.net/taskApi?retryWrites=true', { useNewUrlParser: true });
-mongoose.connection.on("error", function(e) { console.error(e); });
+mongoose.connection.on("error", function (e) { console.error(e); });
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
 
 
-app.get("/todo_items",controller.findAll);
+app.get("/todo_items", controller.findAll);
 app.post("/todo_items", controller.create);
+app.post("/mail", controller.mail)
 app.patch('/todo_items/:id', controller.update);
 app.delete('/todo_items/:id', controller.delete);
 
